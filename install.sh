097e7b8af37046ff48132fbb1b3005479ee548ca
Jonas Kuske <30421456+jonaskuske@users.noreply.github.com>
2020-11-13 05:07:28 +0100

    ci: Introduce CI/CD. Thanks @oxc for your contribution.
    This commit is a combination of 6 commits.
    
    chore: typo fix, update docs, make output coherent
    
    fix: quote PAM_WSL_HELLO_WINPATH to allow spaces
    
    chore: increase version, 1.2.0 was already released
    
    build: make builds reproducable
    
    Check-in Cargo.lock to make sure builds always use the same dependency
    versions.
    
    perf: decrease pam module binary size
    
    Enable cargo Link Time Optimization, and strip debug symbols from the
    binary
    
    build: build with github actions
    
    Requires a little refactoring of Makefiles, so we can build linux and
    windows binaries on different machines.

--- install.sh
+++ install.sh
@@ -30,19 +30,19 @@ check_pam_directory () {
 
 
 if [ `whoami` = "root" ]; then
-  echo "Please run this with normal user instead of root. Aborting"
+  echo "Please run this as normal user instead of root. Aborting."
   exit 1
 fi
 if [ ! -e build/pam_wsl_hello.so ] || \
   [ ! -e build/WindowsHelloAuthenticator/WindowsHelloAuthenticator.exe ] || \
   [ ! -e build/WindowsHelloKeyCredentialCreator/WindowsHelloKeyCredentialCreator.exe ]; then
-    echo "No built binary is found. Buld first before installing"
+    echo "No built binary was found. Build first before installing."
     exit 1
 fi
 
 MNT=/mnt/c
 if [[ ! -e "${MNT}" ]]; then
-  echo "'/mnt/c' is not found. Please input your mount point of C drive to invoke Windows commands."
+  echo "'/mnt/c' was not found. Please input the mount point of your C drive to invoke Windows commands."
   echo -n ": "
   read MNT
 fi
@@ -55,10 +55,10 @@ read PAM_WSL_HELLO_WINPATH
 if [ -z "$PAM_WSL_HELLO_WINPATH" ]; then
   PAM_WSL_HELLO_WINPATH=$DEF_PAM_WSL_HELLO_WINPATH
 fi
-if [ ! -e $PAM_WSL_HELLO_WINPATH ]; then
+if [ ! -e "$PAM_WSL_HELLO_WINPATH" ]; then
   if prompt_yn "'$PAM_WSL_HELLO_WINPATH' does not exist. Create it? [Y/n]" "y"; then
     set -x
-    mkdir -p $PAM_WSL_HELLO_WINPATH
+    mkdir -p "$PAM_WSL_HELLO_WINPATH"
   fi
 fi
 set +x
@@ -70,25 +70,25 @@ set +x
 echo_stage "[2/5] Installing PAM module to the Linux system..."
 SECURITY_PATH="/lib/x86_64-linux-gnu/security" 
 if ! check_pam_directory "${SECURITY_PATH}"; then
-  echo "PAM directory is not found in '/lib/x86_64-linux-gnu/security/'. It looks you're not running Ubuntu nor Debian."
+  echo "PAM directory was not found in '/lib/x86_64-linux-gnu/security/'. It looks like you're not running Ubuntu nor Debian."
   echo "Checking '/lib/security/'..."
   SECURITY_PATH="/lib/security" 
   while ! check_pam_directory "${SECURITY_PATH}"; do
-    echo "PAM module directory is not found in '${SECURITY_PATH}'."
+    echo "PAM module directory was not found in '${SECURITY_PATH}'."
     echo "Please input the path of the PAM module's directory."
     echo -n ": "
     read SECURITY_PATH
   done
 fi
-echo "Confirmed the '${SECURITY_PATH}' is the PAM module directory."
+echo "Confirmed '${SECURITY_PATH}' as the PAM module directory."
 PAM_SO="${SECURITY_PATH}/pam_wsl_hello.so"
 if [ -e "${PAM_SO}" ]; then
-  if prompt_yn "'${PAM_SO}' is in use. Do you proceed to remove the current? [Y/n]" "y"; then 
+  if prompt_yn "'${PAM_SO}' is in use. Proceed to remove the current one? [Y/n]" "y"; then 
     set -x
     sudo rm "${PAM_SO}"
     set +x
   else
-    echo "Installation is cancelled. You can rerun this install.sh after that."
+    echo "Installation was cancelled. You can rerun this with install.sh later."
     exit
   fi
 fi
@@ -98,7 +98,7 @@ sudo chown root:root "${SECURITY_PATH}/pam_wsl_hello.so"
 sudo chmod 644 "${SECURITY_PATH}/pam_wsl_hello.so"
 
 set +x
-echo_stage "[3/5] Createing the config files of WSL-Hello-sudo..."
+echo_stage "[3/5] Creating the config files of WSL-Hello-sudo..."
 set -x
 sudo mkdir -p /etc/pam_wsl_hello/
 set +x
@@ -107,19 +107,19 @@ if [ ! -e "/etc/pam_wsl_hello/config" ] || prompt_yn "'/etc/pam_wsl_hello/config
   sudo touch /etc/pam_wsl_hello/config
   sudo echo "authenticator_path = \"$PAM_WSL_HELLO_WINPATH/WindowsHelloAuthenticator/WindowsHelloAuthenticator.exe\"" | sudo tee /etc/pam_wsl_hello/config
 else
-  echo "skip creation of '/etc/pam_wsl_hello/config'"
+  echo "Skipping creation of '/etc/pam_wsl_hello/config'..."
 fi
 echo "Please authenticate yourself now to create a credential for '$USER' and '$WINUSER' pair."
 KEY_ALREADY_EXIST_ERR=170
 set -x
-pushd $PAM_WSL_HELLO_WINPATH
+pushd "$PAM_WSL_HELLO_WINPATH"
 WindowsHelloKeyCredentialCreator/WindowsHelloKeyCredentialCreator.exe pam_wsl_hello_$USER|| test $? = $KEY_ALREADY_EXIST_ERR
 sudo mkdir -p /etc/pam_wsl_hello/public_keys
 popd
 sudo cp "$PAM_WSL_HELLO_WINPATH"/pam_wsl_hello_$USER.pem /etc/pam_wsl_hello/public_keys/
 
 set +x
-echo_stage "[4/5] Createing uninstall.sh..."
+echo_stage "[4/5] Creating uninstall.sh..."
 if [ ! -e "uninstall.sh" ] || prompt_yn "'uninstall.sh' already exists. Overwrite it? [Y/n]" "y" ; then
   cat > uninstall.sh << EOS
   echo -e "\e[31mNote: Please ensure that config files in /etc/pam.d/ are restored to as they were before WSL-Hello-sudo was installed\e[m"
@@ -130,7 +130,7 @@ if [ ! -e "uninstall.sh" ] || prompt_yn "'uninstall.sh' already exists. Overwrit
 EOS
   chmod +x uninstall.sh
 else
-  echo "skip creation of 'uninstall.sh'"
+  echo "Skipping creation of 'uninstall.sh'..."
 fi
 set -x
 set +x
